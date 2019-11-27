aed14082066fe94ea7309d408050417783e95604
unknown <dcorteswebdev@gmail.com>
2019-11-27 17:50:43 -0600

    feat: Added Search bar

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import SearchScreen from './src/screens/SearchScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const navigator = createStackNavigator({
+  Search: SearchScreen
+}, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'BusinessSearch'
+  }
+})
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+export default createAppContainer(navigator)
+
+// yGr1XhhAuCgtl7kdkM8QWQ
+
+// d0wA6NYuWiNnQMMgCmP4FpZQODut69ME8FcyE8IaldAExm8AJCUXtiNd52NUZ6q-dNaSN73BXRrQ55ZYEN84GzUGy8RJTpHeceV_xo_Ep0L9o-OQu2rMXkxwivveXXYx
