e378d169ed139b3c548ff75817b292e73ec5bb03
unknown <dcorteswebdev@gmail.com>
2019-11-29 11:52:16 -0600

    feat: Finished App

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import SearchBar from '../compnents/SearchBar'
 import useResults from '../hooks/useResults'
 import ResultsList from '../compnents/ResultsList'
@@ -15,26 +15,28 @@ const SearchScreen = () => {
   }
 
   return (
-    <View>
+    <>
       <SearchBar 
         term={term} 
         onTermChange={(newTerm) => setTerm(newTerm)}
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>We have found {results.length} results</Text>
-      <ResultsList 
-        title="Cost Effective"
-        results={filterResultsByPrice('$')}/>
-      <ResultsList 
-        title="Bit Pricier"
-        results={filterResultsByPrice('$$')}
-      />
-      <ResultsList 
-        title="Big Spender"
-        results={filterResultsByPrice('$$$')}
-      />
-    </View>
+      <ScrollView>
+        <ResultsList 
+          title="Cost Effective"
+          results={filterResultsByPrice('$')}
+        />
+        <ResultsList 
+          title="Bit Pricier"
+          results={filterResultsByPrice('$$')}
+        />
+        <ResultsList 
+          title="Big Spender"
+          results={filterResultsByPrice('$$$')}
+        />
+      </ScrollView>
+    </>
   )
 }
 
